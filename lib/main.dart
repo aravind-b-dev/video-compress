66f76b105d1b3d7b7cbc8a831a884044f7e345b1
aravind-b-python <baravind460@gmail.com>
2021-12-05 16:18:32 +0530

    added progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:video_compress/video_compress.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,68 +17,80 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  VdoCompress(),
+      home: VdoCompress(),
     );
   }
 }
 
-
-
-
-
 class VdoCompress extends StatefulWidget {
-
   @override
   _VdoCompressState createState() => _VdoCompressState();
 }
 
 class _VdoCompressState extends State<VdoCompress> {
   var vdoPath;
+  late Subscription _subscription;
+  var progres = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _subscription = VideoCompress.compressProgress$.subscribe((progress) {
+      debugPrint('progress: $progress');
+      setState(() {
+        progres = progress;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Video Compress"),
-          leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: Icon(Icons.arrow_back_ios),
-          ),
+      appBar: AppBar(
+        title: const Text("Video Compress"),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(
+                    type: FileType.video,
+                  );
+                  print("----->>>---${result!.files.single.path}");
+                  if (result != null) {
+                    setState(() {
+                      vdoPath = result.files.single.path;
+                    });
+                  } else {
+                    //do somthing
+                  }
+                },
+                child: const Text("Pick video")),
+
+            SizedBox(height: 25,),
+            ElevatedButton(
+                onPressed: () {
+                  vdoCompress();
+                },
+                child: const Text("Compress")),
+
+            SizedBox(height: 25,),
+
+            Text("Progress : $progres",textAlign: TextAlign.center,),
+          ],
         ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-
-            children: [
-              ElevatedButton(
-                  onPressed: () async {
-                    FilePickerResult? result = await FilePicker.platform.pickFiles(
-                      type: FileType.video,
-                    );
-                    print("----->>>---${result!.files.single.path}");
-                    if(result != null) {
-
-                      setState(() {
-                        vdoPath = result.files.single.path;
-                      });
-                    } else {
-                      //do somthing
-                    }
-                  },
-                  child: const Text("Pick video")),
-
-              ElevatedButton(
-                  onPressed: () {
-                    vdoCompress();
-                  },
-                  child: const Text("Compress"))
-            ],
-          ),
-        ),
+      ),
     );
   }
 
-  vdoCompress () async {
+  vdoCompress() async {
     MediaInfo? mediaInfo = await VideoCompress.compressVideo(
       vdoPath,
       quality: VideoQuality.LowQuality,
@@ -85,5 +98,4 @@ class _VdoCompressState extends State<VdoCompress> {
     );
     print("----------$mediaInfo");
   }
-
 }
